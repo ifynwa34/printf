6dedbb795de70fdcb8cfa4c933891e8f906f377e
tobialism <tobialism@gmail.com>
2023-09-18 16:38:39 -0700

    printf udated

--- handle_print.c
+++ handle_print.c
@@ -1,67 +1,49 @@
 #include "main.h"
-
 /**
- * _printf - Custom printf function
- * @format: The format string
- * @...: Variable number of arguments
- *
- * Return: Number of characters printed
+ * handle_print - Prints an argument based on its type
+ * @fmt: Formatted string in which to print the arguments.
+ * @list: List of arguments to be printed.
+ * @ind: ind.
+ * @buffer: Buffer array to handle print.
+ * @flags: Calculates active flags
+ * @width: get width.
+ * @precision: Precision specification
+ * @size: Size specifier
+ * Return: 1 or 2;
  */
-int _printf(const char *format, ...)
+int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
+	int flags, int width, int precision, int size)
 {
-    va_list args;
-    int printed_chars = 0;
-    int i = 0;
+	int i, unknow_len = 0, printed_chars = -1;
+	fmt_t fmt_types[] = {
+		{'c', print_char}, {'s', print_string}, {'%', print_percent},
+		{'i', print_int}, {'d', print_int}, {'b', print_binary},
+		{'u', print_unsigned}, {'o', print_octal}, {'x', print_hexadecimal},
+		{'X', print_hexa_upper}, {'p', print_pointer}, {'S', print_non_printable},
+		{'r', print_reverse}, {'R', print_rot13string}, {'\0', NULL}
+	};
+	for (i = 0; fmt_types[i].fmt != '\0'; i++)
+		if (fmt[*ind] == fmt_types[i].fmt)
+			return (fmt_types[i].fn(list, buffer, flags, width, precision, size));
 
-    va_start(args, format);
-
-    while (format && format[i])
-    {
-        if (format[i] != '%')
-        {
-            putchar(format[i]);
-            printed_chars++;
-        }
-        else if (format[i + 1])
-        {
-            i++;
-            switch (format[i])
-            {
-                case 'd':
-                    printed_chars += printf("%d", va_arg(args, int));
-                    break;
-                case 's':
-                    printed_chars += printf("%s", va_arg(args, char *));
-                    break;
-                case 'c':
-                    putchar(va_arg(args, int));
-                    printed_chars++;
-                    break;
-                case 'f':
-                    printed_chars += printf("%f", va_arg(args, double));
-                    break;
-                default:
-                    putchar('%');
-                    putchar(format[i]);
-                    printed_chars += 2;
-                    break;
-            }
-        }
-        i++;
-    }
-
-    va_end(args);
-    return (printed_chars);
-}
-
-int main(void)
-{
-    int i = 42;
-    char *s = "Hello, world!";
-    char c = 'A';
-    double f = 3.14159;
-
-    _printf("Integer: %d, String: %s, Char: %c, Float: %f\n", i, s, c, f);
-
-    return (0);
+	if (fmt_types[i].fmt == '\0')
+	{
+		if (fmt[*ind] == '\0')
+			return (-1);
+		unknow_len += write(1, "%%", 1);
+		if (fmt[*ind - 1] == ' ')
+			unknow_len += write(1, " ", 1);
+		else if (width)
+		{
+			--(*ind);
+			while (fmt[*ind] != ' ' && fmt[*ind] != '%')
+				--(*ind);
+			if (fmt[*ind] == ' ')
+				--(*ind);
+			return (1);
+		}
+		unknow_len += write(1, &fmt[*ind], 1);
+		return (unknow_len);
+	}
+	return (printed_chars);
 }
