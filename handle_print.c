19283319ffe806095c537f4679c5d471d6c3c9b0
tobialism <tobialism@gmail.com>
2023-09-19 10:47:11 -0700

    printf updated

--- handle_print.c
+++ handle_print.c
@@ -1,109 +1,49 @@
 #include "main.h"
-
 /**
- * print_with_precision - Helper function to print with precision.
- * @specifier: The conversion specifier ('d', 's', etc.).
- * @list: List of arguments to be printed.
- * @buffer: Buffer array to handle print.
- * @flags: Calculates active flags.
- * @width: Get width.
- * @precision: Precision specification.
- * @size: Size specifier.
- * Return: Number of characters printed.
- */
-static int print_with_precision(char specifier, va_list list, char buffer[],
-    int flags, int width, int precision, int size)
-{
-    int printed_chars = 0;
-    switch (specifier)
-    {
-        case 'd':
-            printed_chars += print_int(list, buffer, flags, width, precision, size);
-            break;
-        case 's':
-            printed_chars += print_string(list, buffer, flags, width, precision, size);
-            break;
-        case 'c':
-        case '%':
-        case 'i':
-        case 'b':
-        case 'u':
-        case 'o':
-        case 'x':
-        case 'X':
-        case 'p':
-        case 'S':
-        case 'r':
-        case 'R':
-            printed_chars += fmt_types[i].fn(list, buffer, flags, width, -1, size);
-            break;
-    }
-    return printed_chars;
-}
-
-/**
- * handle_print - Prints an argument based on its type.
+ * handle_print - Prints an argument based on its type
  * @fmt: Formatted string in which to print the arguments.
- * @ind: Index to keep track of the format string position.
  * @list: List of arguments to be printed.
+ * @ind: ind.
  * @buffer: Buffer array to handle print.
- * Return: Number of characters printed or -1 on error.
+ * @flags: Calculates active flags
+ * @width: get width.
+ * @precision: Precision specification
+ * @size: Size specifier
+ * Return: 1 or 2;
  */
-int handle_print(const char *fmt, int *ind, va_list list, char buffer[])
+int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
+	int flags, int width, int precision, int size)
 {
-    int i, unknown_len = 0;
-    int printed_chars = 0;
+	int i, unknow_len = 0, printed_chars = -1;
+	fmt_t fmt_types[] = {
+		{'c', print_char}, {'s', print_string}, {'%', print_percent},
+		{'i', print_int}, {'d', print_int}, {'b', print_binary},
+		{'u', print_unsigned}, {'o', print_octal}, {'x', print_hexadecimal},
+		{'X', print_hexa_upper}, {'p', print_pointer}, {'S', print_non_printable},
+		{'r', print_reverse}, {'R', print_rot13string}, {'\0', NULL}
+	};
+	for (i = 0; fmt_types[i].fmt != '\0'; i++)
+		if (fmt[*ind] == fmt_types[i].fmt)
+			return (fmt_types[i].fn(list, buffer, flags, width, precision, size));
 
-    for (i = 0; fmt_types[i].fmt != '\0'; i++)
-    {
-        if (fmt[*ind] == fmt_types[i].fmt)
-        {
-            // Handle precision for %d and %s
-            if (fmt[*ind] == 'd' || fmt[*ind] == 's')
-            {
-                // Check and apply precision
-                int precision = -1; // Initialize precision to -1
-                if (fmt[*ind] == 'd' || fmt[*ind] == 's')
-                {
-                    if (fmt[*ind + 1] == '.')
-                    {
-                        *ind += 2;
-                        precision = 0;
-                        while (fmt[*ind] >= '0' && fmt[*ind] <= '9')
-                        {
-                            precision = precision * 10 + (fmt[*ind] - '0');
-                            (*ind)++;
-                        }
-                    }
-                }
-                printed_chars += print_with_precision(fmt[*ind], list, buffer, flags, width, precision, size);
-            }
-            else
-            {
-                printed_chars += fmt_types[i].fn(list, buffer, flags, width, -1, size);
-            }
-            return printed_chars;
-        }
-    }
-
-    if (fmt_types[i].fmt == '\0')
-    {
-        if (fmt[*ind] == '\0')
-            return (-1);
-        unknown_len += write(1, "%%", 1);
-        if (fmt[*ind - 1] == ' ')
-            unknown_len += write(1, " ", 1);
-        else if (width)
-        {
-            --(*ind);
-            while (fmt[*ind] != ' ' && fmt[*ind] != '%')
-                --(*ind);
-            if (fmt[*ind] == ' ')
-                --(*ind);
-            return (1);
-        }
-        unknown_len += write(1, &fmt[*ind], 1);
-        return (unknown_len);
-    }
-    return (printed_chars);
+	if (fmt_types[i].fmt == '\0')
+	{
+		if (fmt[*ind] == '\0')
+			return (-1);
+		unknow_len += write(1, "%%", 1);
+		if (fmt[*ind - 1] == ' ')
+			unknow_len += write(1, " ", 1);
+		else if (width)
+		{
+			--(*ind);
+			while (fmt[*ind] != ' ' && fmt[*ind] != '%')
+				--(*ind);
+			if (fmt[*ind] == ' ')
+				--(*ind);
+			return (1);
+		}
+		unknow_len += write(1, &fmt[*ind], 1);
+		return (unknow_len);
+	}
+	return (printed_chars);
 }
